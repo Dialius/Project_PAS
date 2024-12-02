ebe60d6f6c0c77a8280636edd800e2e88b4fa98e
Dialius <144961003+Dialius@users.noreply.github.com>
2024-12-02 19:10:31 +0700

    Update Register.js

--- Register.js
+++ Register.js
@@ -48,7 +48,7 @@ signUp.addEventListener('click', (event)=> {
         const docRef=doc(db, "users", user.uid);
         setDoc(docRef,userData)
         .then(()=>{
-            window.location.href='Login.html';
+            window.location.href='login.html';
         })
         .catch((error)=>{
             console.error("error writing document", error);
